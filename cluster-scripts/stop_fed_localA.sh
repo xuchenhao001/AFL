8988f99308f8699dda0682b737d2fc27eb6e0ced
xuchenhao001 <xu.chenhao@hotmail.com>
2021-05-11 14:32:57 +1000

    bug fix for fed_localA

--- cluster-scripts/stop_fed_localA.sh
+++ cluster-scripts/stop_fed_localA.sh
@@ -5,6 +5,7 @@ source ../fabric-network/network.config
 for i in "${!PeerAddress[@]}"; do
   addrIN=(${PeerAddress[i]//:/ })
   
-  ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[m]ain_fed_localA.py'|awk '{print \$2}')"
+  ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[f]ed_localA.py'|awk '{print \$2}')"
 done
+
 
