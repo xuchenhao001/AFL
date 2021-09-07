45c0f691302cb997c54a8f76b902c1c806871e02
xuchenhao001 <xu.chenhao@hotmail.com>
2021-09-07 16:28:07 +1000

    tune for time-acc

--- cluster-scripts/all_nodes_test_fade_bg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nohup ./all_nodes_test_fade.sh > all_nodes_test_fade.log 2>&1 &
-
---
+++ cluster-scripts/all_nodes_test_static_bg.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nohup ./all_nodes_test_static.sh > all_nodes_test_static.log 2>&1 &
+
